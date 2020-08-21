16a80fe573ba61627168f859310c04c306a2d728
Simao, Guilherme P <guilherme.p.simao@accenture.com>
2020-08-20 22:03:01 -0300

    test cli command added

--- test_sphere.py
+++ test_sphere.py
@@ -1,7 +1,15 @@
 import geom_elements
 import math
-
-def testVolume_calculation():
+# testing  : pytest --log-format="%(asctime)s %(levelname)s %(message)s" --log-date-format="%Y-%m-%d %H:%M:%S" >> test.log
+def test_volume_calculation():
     r = 3
     s = geom_elements.Sphere([0,0,0],r)
     assert s.get_volume() == (4/3) * math.pi * (r**3)
+
+def test_is_inside_method():
+    r = 2
+    p1 = geom_elements.Point([1,1,1])
+    p2 = geom_elements.Point([3,3,3])
+    s = geom_elements.Sphere([0,0,0],r)
+    assert s.is_inside(p1) == True
+    assert s.is_inside(p2) == False
